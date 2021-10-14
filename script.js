0c08cb05442c2e33cbc6a2d375be6c395e39bd1d
ekenerew <ekenerew@gmail.com>
2021-10-14 10:15:31 -0700

    Date Updated

---
+++ script.js
@@ -0,0 +1,12 @@
+let counter = document.getElementById("")
+let count = 0;
+
+function increment() {
+    return count++;
+    counter.innerText = count
+
+}
+funtion decrement() {
+    return count--;
+    counter.innerText = count
+}
